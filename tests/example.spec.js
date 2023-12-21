20f49c1daeb3319cb57de4ba176c926402612d80
RushikeshBBankar <rushikesh1611@gmail.com>
2023-12-22 02:05:02 +0530

    first commit

---
+++ tests/example.spec.js
@@ -0,0 +1,8 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
