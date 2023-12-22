ee711165e12d43932ba1004a55be3d622632071e
RushikeshBBankar <rushikesh1611@gmail.com>
2023-12-22 23:34:55 +0530

    allure headless true

--- playwright.config.js
+++ playwright.config.js
@@ -18,7 +18,7 @@ module.exports = defineConfig({
   ],
   use: {
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     screenshot: "on",
     trace: 'retain-on-failure',
   }
