60a231072ee2b68246b83bd6b2b6ecde1235b02d
RushikeshBBankar <rushikesh1611@gmail.com>
2023-12-23 02:12:43 +0530

    add describe block

--- tests/multipleTest.spec.js
+++ tests/multipleTest.spec.js
@@ -1,27 +1,32 @@
 const { test, expect } = require('@playwright/test');
 
-test('validation', async ({ page }) => {
-        await page.goto('https://www.rahulshettyacademy.com/AutomationPractice/');
-        const show = await page.locator('#displayed-text')
-        
-        await expect(show).toBeVisible();
-        await page.locator('#hide-textbox').click();
+test.describe("Home page", async () => {
 
-        await expect(show).toBeHidden();
 
-})
+        test('validation', async ({ page }) => {
+                await page.goto('https://www.rahulshettyacademy.com/AutomationPractice/');
+                const show = await page.locator('#displayed-text')
 
-test('validation of screen shot', async ({ page }) => {
-        await page.goto('https://www.rahulshettyacademy.com/AutomationPractice/');
-        const show = await page.locator('#displayed-text')
-        
+                await expect(show).toBeVisible();
+                await page.locator('#hide-textbox').click();
 
-        await expect(show).toBeVisible();
-        await page.locator('#displayed-text').screenshot({path: 'partialScreen.png'})
-        await page.screenshot({path: 'screenshot.png'})
-        
-        await page.locator('#hide-textbox').click();
+                await expect(show).toBeHidden();
 
-        await expect(show).toBeHidden();
+        })
+
+        test('validation of screen shot', async ({ page }) => {
+                await page.goto('https://www.rahulshettyacademy.com/AutomationPractice/');
+                const show = await page.locator('#displayed-text')
+
+
+                await expect(show).toBeVisible();
+                await page.locator('#displayed-text').screenshot({ path: 'partialScreen.png' })
+                await page.screenshot({ path: 'screenshot.png' })
+
+                await page.locator('#hide-textbox').click();
+
+                await expect(show).toBeHidden();
+
+        })
 
 })
