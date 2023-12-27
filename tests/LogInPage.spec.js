6312ce2b5f1040815dc8a76f179a353a0c0dc099
RushikeshBBankar <rushikesh1611@gmail.com>
2023-12-28 03:57:22 +0530

    POM test

---
+++ tests/LogInPage.spec.js
@@ -0,0 +1,20 @@
+const{test,expect}= require('@playwright/test')
+const {logIn} = require('../PageObjectModal/logIn')
+
+let page;
+let HomePage;
+test.describe("Home Page",()=>{
+        test.beforeAll(async({browser})=>{
+                page = await browser.newPage();
+                HomePage = new logIn(page);
+                await HomePage.navigate()
+        })
+
+        test('validate the page title',async()=>{
+                await HomePage.pageTitle()
+        })
+
+        test('validate the free link is visible or not',async()=>{
+                await HomePage.freeLink()
+        })
+})
